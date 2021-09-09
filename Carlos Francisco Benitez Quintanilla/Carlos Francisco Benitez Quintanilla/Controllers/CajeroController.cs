d6fdbecc471c6bab21b5e6975f235ece95dfe9d0
CarlosBenitez <87823966+CarlosBenitez01@users.noreply.github.com>
2021-09-09 12:38:41 -0600

    parcial 1

---
+++ Carlos Francisco Benitez Quintanilla/Carlos Francisco Benitez Quintanilla/Controllers/CajeroController.cs
@@ -0,0 +1,41 @@
+﻿using Carlos_Francisco_Benitez_Quintanilla.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carlos_Francisco_Benitez_Quintanilla.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Retiro()
+        {
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult ProcVal(CajeroViewModel cajero)
+        {
+            if (Convert.ToInt32(cajero.Monto) % 5 == 0)
+            {
+                ViewBag.monto = cajero.Monto;
+                return Redirect("/Cajero/Retiro");
+
+            }
+            else
+            {
+                return Redirect("/Cajero/Error");
+            }
+        }
+    }
+}
---
+++ Carlos Francisco Benitez Quintanilla/Carlos Francisco Benitez Quintanilla/Models/ViewModels/CajeroViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carlos_Francisco_Benitez_Quintanilla.Models.ViewModels
+{
+    public class CajeroViewModel
+    {
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public String Monto { get; set; }
+    }
+}
